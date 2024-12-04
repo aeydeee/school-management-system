b254c0715a6c6783e1f2306eb11b859116161c74
Adrian Samuel <aeydeee@gmail.com>
2024-12-04 18:01:28 +0800

    removed the name on teacher model since the user model have it already and connected the teacher to the user

--- teacher/models.py
+++ teacher/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils.text import slugify
 
+from home_auth.models import CustomUser
+
 
 class Address(models.Model):
     address = models.CharField(max_length=255)
@@ -9,12 +11,13 @@ class Address(models.Model):
     zip_code = models.IntegerField()
     country = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f'{self.city} {self.zip_code} {self.country}'
+
 
 # Create your models here.
 class Teacher(models.Model):
     teacher_id = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=100, choices=[('male', 'Male'), ('female', 'Female'), ('others', 'Others')])
     date_of_birth = models.DateField()
     religion = models.CharField(max_length=20)
@@ -24,12 +27,15 @@ class Teacher(models.Model):
     experience = models.CharField(max_length=255)
     teacher_image = models.ImageField(upload_to='teachers/', default='teachers/profile.png')
     slug = models.CharField(max_length=255, unique=True, blank=True)
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+
+    address = models.OneToOneField(Address, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = slugify(f'{self.first_name}-{self.last_name}-{self.teacher_id}')
+        if self.user:  # Make sure user exists before trying to access attributes
+            if not self.slug:
+                self.slug = slugify(f'{self.user.first_name}-{self.user.last_name}-{self.teacher_id}')
         super(Teacher, self).save(*args, **kwargs)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name} ({self.teacher_id})'
+        return f'{self.user.first_name} {self.user.last_name} ({self.teacher_id})'
