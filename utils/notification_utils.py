9e27adc16fbcd86d44a19c34012d0ec32d08af57
Adrian Samuel <aeydeee@gmail.com>
2024-12-04 20:49:38 +0800

    put the create_notif inside the try, common sense tbh

--- utils/notification_utils.py
+++ utils/notification_utils.py
@@ -5,9 +5,8 @@ from school.models import Notification
 def create_notification(user, message):
     try:
         user = CustomUser.objects.get(username=user.username)  # Replace 'user_id' with actual ID
+        create_notif = Notification.objects.create(user=user, message=message)
     except CustomUser.DoesNotExist:
         raise ValueError("User does not exist.")
-
-    create_notif = Notification.objects.create(user=user, message=message)
 
     return create_notif
