73b8f780ef826c88779658fa4bc36d5941d05de0
Adrian Samuel <aeydeee@gmail.com>
2024-12-04 17:46:36 +0800

    make a notification_context_processor to be called globally anywhere in the template

--- context_processors.py
+++ context_processors.py
@@ -5,17 +5,30 @@ from school.models import Notification
 
 
 def add_active_page(request):
+    print(f"Current URL Name: {request.resolver_match.url_name}")
     return {'active_page': request.resolver_match.url_name}
 
 
 def notification_context_processor(request):
-    unread_notification = Notification.objects.filter(user=request.user, is_read=False)
-    unred_notif = unread_notification.count()
+    unred_notif = 0
+    unread_notification = None
+    all_notifications = Notification.objects.all()
+    notifications = Notification.objects.filter(user=request.user.id)
+    if request.user:
+        try:
+            unread_notification = Notification.objects.filter(user=request.user.id, is_read=False)
+            unred_notif = unread_notification.count()
+        except Notification.DoesNotExist:
+            unred_notif = 0
+        except Exception as e:
+            print(f"Error processing notification: {e}")
+            unred_notif = 0
+    else:
+        unred_notif = 0
 
     # This part should not be reachable after the above return statements.
     return {
         'unread_notification_count': unred_notif,
-        'unread_notification': unread_notification
+        'notifications': notifications,
+        'all_notifications': all_notifications
     }
-
-
