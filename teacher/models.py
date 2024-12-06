96a8e209dac8e1142e9cd3e12da5d18e94a9da8c
Adrian Samuel <aeydeee@gmail.com>
2024-12-06 14:09:09 +0800

    added a function to get the full name of the teacher

--- teacher/models.py
+++ teacher/models.py
@@ -39,3 +39,7 @@ class Teacher(models.Model):
 
     def __str__(self):
         return f'{self.user.first_name} {self.user.last_name} ({self.teacher_id})'
+
+    @property
+    def full_name(self):
+        return f'{self.user.first_name} {self.user.last_name}'
