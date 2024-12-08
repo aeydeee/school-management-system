01cb7e1af0da559cb4badd48f2445621bd7cde6c
Adrian Samuel <aeydeee@gmail.com>
2024-12-08 14:19:00 +0800

    created a subject and class models

--- subject/models.py
+++ subject/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from department.models import Department
+from teacher.models import Teacher
+
+
 # Create your models here.
+class Subject(models.Model):
+    code = models.CharField(max_length=45)
+    name = models.CharField(max_length=100)
+
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='subjects')
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=100)
+
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE, related_name='classes')
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='classes')
