78f4e0f205503cd2be06c96783fdd494980592b9
Adrian Samuel <aeydeee@gmail.com>
2024-12-09 11:35:46 +0800

    added a urls for add and edit for subjects

--- subject/urls.py
+++ subject/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from subject.views import SubjectListView
+from subject.views import SubjectListView, SubjectCreateView, SubjectUpdateView
 
 app_name = 'subject'
 
 urlpatterns = [
     path('', SubjectListView.as_view(), name='subject_list'),
+    path('add/', SubjectCreateView.as_view(), name='add_subject'),
+    path('edit/<int:pk>/', SubjectUpdateView.as_view(), name='edit_subject'),
 ]
