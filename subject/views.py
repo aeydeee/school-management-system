e869776a1f4f4e13c5c292dd17fb38a32dd723ac
Adrian Samuel <aeydeee@gmail.com>
2024-12-09 11:36:27 +0800

    created a create and update view using generic CBVs for subject

--- subject/views.py
+++ subject/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
 
+from subject.forms import SubjectForm
 from subject.models import Subject
 
 
@@ -10,3 +12,17 @@ class SubjectListView(ListView):
     template_name = 'subjects/subjects.html'
     context_object_name = 'subjects'
     ordering = ['-id']
+
+
+class SubjectCreateView(CreateView):
+    model = Subject
+    template_name = 'subjects/add-subject.html'
+    form_class = SubjectForm
+    success_url = reverse_lazy('subject:subject_list')
+
+
+class SubjectUpdateView(UpdateView):
+    model = Subject
+    form_class = SubjectForm
+    template_name = 'subjects/edit-subject.html'
+    success_url = reverse_lazy('subject:subject_list')
