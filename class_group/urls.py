6fd56b62a432a85e8acecfadf875bbe8a86fa7e1
Adrian Samuel <aeydeee@gmail.com>
2024-12-12 19:30:27 +0800

    added the urls and views for my class app

--- class_group/urls.py
+++ class_group/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 
-from class_group.views import ClassListView, ClassCreateView
+from class_group.views import ClassListView, ClassCreateView, ClassUpdateView, SectionUpdateView, SectionManageView
 
 app_name = 'class'
 
 urlpatterns = [
     path('', ClassListView.as_view(), name='class_list'),
     path('add/', ClassCreateView.as_view(), name='add_class'),
+    path('edit/<int:pk>/', ClassUpdateView.as_view(), name='edit_class'),
+
+    path('section/', SectionManageView.as_view(), name='section_list'),
+    path('section/edit/<int:pk>/', SectionUpdateView.as_view(), name='edit_section'),
 ]
