2c08bf0edc50ece6b6d490333525522e322bf405
Adrian Samuel <aeydeee@gmail.com>
2024-12-12 19:27:53 +0800

    created an admin on class_group app for class and section models

--- class_group/admin.py
+++ class_group/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from class_group.models import Class, Section
+
+
 # Register your models here.
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ['name', 'subject', 'section']
+    list_filter = ('name', 'subject')
+    search_fields = ['subject', 'name', 'section']
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ['grade_level', 'name']
+    list_filter = ('name', 'grade_level')
+    search_fields = ['grade_level', 'name']
