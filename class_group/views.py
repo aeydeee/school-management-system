0d8f8a924c5b778127c50f3875ac640cadae337b
Adrian Samuel <aeydeee@gmail.com>
2024-12-12 19:30:39 +0800

    added the urls and views for my class app

--- class_group/views.py
+++ class_group/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView, View
 
-from class_group.forms import ClassForm
-from class_group.models import Class
+from class_group.forms import ClassForm, SectionForm
+from class_group.models import Class, Section
 
 
 # Create your views here.
@@ -19,3 +19,34 @@ class ClassCreateView(CreateView):
     form_class = ClassForm
     template_name = 'class_groups/add-class.html'
     success_url = reverse_lazy('class:class_list')
+
+
+class ClassUpdateView(UpdateView):
+    model = Class
+    form_class = ClassForm
+    template_name = 'class_groups/edit-class.html'
+    success_url = reverse_lazy('class:class_list')
+
+
+class SectionManageView(View):
+    template_name = 'class_groups/section/sections.html'
+
+    def get(self, request, *args, **kwargs):
+        sections = Section.objects.order_by('grade_level', 'name')
+        form = SectionForm()
+        return render(request, self.template_name, {'sections': sections, 'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = SectionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('class:section_list')
+        sections = Section.objects.order_by('grade_level', 'name')
+        return render(request, self.template_name, {'sections': sections, 'form': form})
+
+
+class SectionUpdateView(UpdateView):
+    model = Section
+    form_class = SectionForm
+    template_name = 'class_groups/section/sections.html'
+    success_url = reverse_lazy('class:section_list')
