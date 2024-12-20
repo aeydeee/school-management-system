7603732cc48b063d8c9cc16303057a9f1d05210e
Adrian Samuel <aeydeee@gmail.com>
2024-12-20 10:42:00 +0800

    move from sqlite to mysql

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -85,10 +85,18 @@ WSGI_APPLICATION = 'student_management_system.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'student_management_system',
+        'USER': 'techninjas',
+        'PASSWORD': 'techyninjas',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+
     }
+
 }
 
 # Password validation
